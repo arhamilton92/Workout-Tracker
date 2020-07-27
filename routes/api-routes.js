adcbd3c961a548b526e176ec14a8608b5b81ab0b
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-27 19:28:18 -0400

    Had a bunch of copied over code from other students trying to figure out that error. Removed the code that was not mine, and filled out api-routes.js. heroku deploymeny now works.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,47 +1,46 @@
 
-const db = require("../models");
+// DEPENDENCIES ---------------------
+const db = require('../models');
+const express = require('express');
+const router = express.Router();
+// ---------------------------------^
 
-module.exports = function(app) {
 
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-    .then((dbWorkouts) => {
-        res.json(dbWorkouts);
-    }).catch(err => {
+// ROUTES ------------------------------------------------------------------------------------------------------
+// gets all workouts -----------------------------
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then((allWorkouts) => {
+        res.json(allWorkouts);
+    });
+});
+// ----------------------------------------------^
+
+
+// post new workout ---------------------------------
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body).then(newWorkout => {
+      res.json(newWorkout)
+  }).catch(err => {
       console.log(err);
-      res.status(500);
+      res.json({
+          error: true,
+          data: null,
+          message: "error"
+      })
+  });
 });
-});
+// -------------------------------------------------^
 
-app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({})
-    .limit(7)
-    .then((dbWorkouts) => {
-        res.json(dbWorkouts);
-        console.log(dbWorkouts);
-    });
-});
 
-app.put("/api/workouts/:id", (req, res) => {
+// update workouts ----------------------------------------------------------------------------------------
+router.put("/api/workouts/:id", (req, res) => {
     const id = req.params.id;
-    console.log(id);
-    console.log(req.body);
-    db.Workout.findByIdAndUpdate(id, {$push: {"exercises": req.body}},{ new: true }).then((dbWorkouts) => {
-        res.json(dbWorkouts);
+    db.Workout.findByIdAndUpdate(id, {$push: {"exercises": req.body}}, {"new": true}).then((update) => {
+        res.json(update);
     });
 });
+// -------------------------------------------------------------------------------------------------------^
+// ------------------------------------------------------------------------------------------------------------^
 
-app.post("/api/workouts", (req, res) => {  
-    db.Workout.create(req.body).then(dbWorkouts => {
-        res.json(dbWorkouts);
-        console.log(dbWorkouts, "Successfully created workout");
-    }).catch(err => {
-        console.log(err);
-        res.json({
-            error: true,
-            data: null,
-            message: "Workout Failed."
-        });
-    });
-});
-};
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const app = express();
 const htmlRoutes = require('./routes/html-routes.js');
 const apiRoutes = require('./routes/api-routes.js');
-let db = require('./models');
 // -------------------------------------^
 
 
