5b7e0ffe13d1c47e3cf8a0facc9b8a16ab685cd5
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-27 17:48:15 -0400

    stupid heroku error

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const fs = require('fs');
 const router = express.Router();
 const db = require('../models');
 
-router.get('/api/workouts', (req, res) => {
-    db.Workout.find()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500);
-    })
-});
+// router.get('/api/workouts', (req, res) => {
+//     db.Workout.find({})
+//     .then(data => {
+//         res.json(data);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500);
+//     })
+// });
 
 module.exports = router;
