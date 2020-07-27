c99758142a1d971ef4ebed8fdfab67f77d94c0cc
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-27 17:51:09 -0400

    heroku error

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require('../models');
 
-// router.get('/api/workouts', (req, res) => {
-//     db.Workout.find({})
-//     .then(data => {
-//         res.json(data);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500);
-//     })
-// });
+router.get('/api/workouts', (req, res) => {
+    db.Workout.find({})
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+    })
+});
 
 module.exports = router;
