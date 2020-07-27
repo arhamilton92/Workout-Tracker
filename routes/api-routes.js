02bb5d59cfdd4e7e96bb9f4741605a75fbb4e04a
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-27 17:01:16 -0400

    still error

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,17 +1,70 @@
 const express = require("express");
-const fs = require('fs');
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
 
-router.get('/api/workouts', (req, res) => {
-    db.Workout.find()
-    .then(data => {
-        res.json(data);
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((foundWorkouts) => {
+      res.json({
+        error: false,
+        data: foundWorkouts,
+        message: "All workouts retrieved.",
+      });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(500);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to retrieve all workouts.",
+      });
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create({})
+    .then((createWorkouts) => {
+      res.json({
+        error: false,
+        data: createWorkouts,
+        message: "Workout created.",
+      });
     })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to create workout.",
+      });
+    });
+});
+
+router.put("api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $push: { exercises: req.body },
+      $inc: { totalDuration: req.body.duration },
+    },
+    { new: true }
+  )
+    .then((updateWorkouts) => {
+      res.json({
+        error: false,
+        data: updateWorkouts,
+        message: "Workout updated.",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to update workout.",
+      });
+    });
 });
 
 module.exports = router;
