2b0601f6c7a7d4be7634f1821182b3614b9d6263
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-27 16:58:55 -0400

    still trying to fix heroku error

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,19 +2,9 @@ const express = require("express");
 const fs = require('fs');
 const router = express.Router();
 const db = require('../models');
-const path = require('path');
-
-router.get("/api/config", (req, res) => {
-    db.Workout.find({}).then((foundWorkouts) => {
-        res.json({
-            success: true,
-            data: foundWorkouts,
-        });
-    });
-});
 
 router.get('/api/workouts', (req, res) => {
-    db.Workout.find({})
+    db.Workout.find()
     .then(data => {
         res.json(data);
     })
